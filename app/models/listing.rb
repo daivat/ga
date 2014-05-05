90d0bd37974e567b4d461643dc2b53d62fb23534
daivatddholakia <daivatddholakia@gmail.com>
2014-05-04 21:55:51 -0700

    Changed default listing image

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ActiveRecord::Base
 
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "https://lh6.ggpht.com/ifiIqmTG7AFXEqbxiBlHn2BD18ufYcQvg_QdqtZPuyIycYGJtNXtniTzSba4XkvLdA=w300"
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "https://s3.amazonaws.com/Gangle/glassangle_logo_stacked.jpg"
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
 	validates :title, presence: true, length: { minimum: 3 }
 	validates :description, presence: true
